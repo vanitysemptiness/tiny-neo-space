a41256ec3b21f8cd1a8bbbc439bead12fa49fab3
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-07-24 09:25:06 -0400

    doc: added bug to planning.txt and commented out mouse dragging feature

--- src/app/dragging.rs
+++ src/app/dragging.rs
@@ -6,7 +6,7 @@ use super::{drawing::segment_distance, ApplicationState};
 
 pub fn perform_dragging(state: &mut ApplicationState, mouse_pos: &Vec2) {
     if is_mouse_button_pressed(MouseButton::Left) {
-        //TODO: behavior removed until canvas is once again infinite
+        //TODO: behavior removed until canvas is once again infinite 
         // state.highlighted = state.lines.iter().position(|&(start, end)| {
         //     let dist = segment_distance(start, end, mouse_pos.clone());
         //     dist < 5.0
